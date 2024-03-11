e759d8026528c66cf45d68d7682723da3f2221cd
hazadus <hazadus7@gmail.com>
2024-03-11 23:47:18 +0300

    dev: Support all four basic math operations in expression.

--- interpreter_ex1.py
+++ interpreter_ex1.py
@@ -1,7 +1,8 @@
 """
-Basic interpreter of expressions like "X+Y", where X and Y – integers.
+Basic interpreter of expressions like "X+Y", "X-Y", "X*Y", "X/Y", where X and Y – integers.
 """
 
+import operator
 from enum import Enum
 
 
@@ -13,6 +14,9 @@ class Token:
     class Type(str, Enum):
         INTEGER = "INTEGER"
         PLUS = "PLUS"
+        MINUS = "MINUS"
+        MULTIPLY = "MULTIPLY"
+        DIVIDE = "DIVIDE"
         EOF = "EOF"
 
     def __init__(self, type_: Type, value):
@@ -62,26 +66,35 @@ class Interpreter:
             return Token(Token.Type.INTEGER, int(number))
 
         # Try to parse operation
-        if current_char == "+":
+        if current_char in ["+", "-", "*", "/"]:
             self.pos += 1
-            return Token(Token.Type.PLUS, current_char)
+            symbol_to_type = {
+                "+": Token.Type.PLUS,
+                "-": Token.Type.MINUS,
+                "*": Token.Type.MULTIPLY,
+                "/": Token.Type.DIVIDE,
+            }
+            return Token(symbol_to_type[current_char], current_char)
 
         self._error()
 
-    def _eat(self, token_type: Token.Type):
+    def _eat(self, token_type: Token.Type | list[Token.Type]):
         """
         Compare the current token type with the passed token type and if they match then "eat" the current token
         and assign the next token to the self.current_token, otherwise raise an exception.
         """
-        if self.current_token.type_ == token_type:
+        if (
+            isinstance(token_type, Token.Type)
+            and self.current_token.type_ == token_type
+        ) or (isinstance(token_type, list) and self.current_token.type_ in token_type):
             self.current_token = self._get_next_token()
         else:
             self._error()
 
     def expr(self) -> int:
         """
-        Try to parse and evaluate the only expression we know: "X+Y", where X and Y are integers.
-        Expected structure to find: INTEGER -> PLUS -> INTEGER
+        Try to parse and evaluate expression we know.
+        Expected structure to find: INTEGER -> PLUS | MINUS | MULTIPLY | DIVIDE -> INTEGER
         :return: result of parsed expression
         """
         # Parse the first token
@@ -91,16 +104,26 @@ class Interpreter:
         left = self.current_token
         self._eat(Token.Type.INTEGER)
 
-        # Expect "+"
+        # Expect "+" | "-" | "*" | "/"
         op = self.current_token
-        self._eat(Token.Type.PLUS)
+        self._eat(
+            [Token.Type.PLUS, Token.Type.MINUS, Token.Type.MULTIPLY, Token.Type.DIVIDE]
+        )
 
         # Expect second digit
         right = self.current_token
         self._eat(Token.Type.INTEGER)
         # NB: After the last call, current_token is set to EOF
 
-        result = left.value + right.value
+        # "Translate" operation token type to Python built-in operation, and get the result
+        op_to_operator = {
+            Token.Type.PLUS: operator.add,
+            Token.Type.MINUS: operator.sub,
+            Token.Type.MULTIPLY: operator.mul,
+            Token.Type.DIVIDE: operator.truediv,
+        }
+        operation = op_to_operator[op.type_]
+        result = operation(left.value, right.value)
         return result
 
 
